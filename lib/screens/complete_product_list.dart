fcf8113e7a2f4105edf77fb7b421e65b77be43f9
Guspascad420 <Paacad@gmail.com>
2023-10-19 23:23:23 +0700

    Updated app methods

--- lib/screens/complete_product_list.dart
+++ lib/screens/complete_product_list.dart
@@ -1,13 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:vando/screens/main/home_screen.dart';
+import 'package:vando/screens/shopping_cart.dart';
+import 'package:vando/utils/scroll_to_hide_widget.dart';
 import 'package:vando/utils/static_grid.dart';
 import '../models/database_service.dart';
 import '../models/product.dart';
+import '../models/users.dart';
+import '../utils/reusable_widgets.dart';
 
 class CompleteProductList extends StatefulWidget {
-  const CompleteProductList({super.key});
+  const CompleteProductList(
+      {super.key,
+      required this.favProducts,
+      required this.isProductsOnCart,
+      required this.productsOnCartCount,
+      required this.productsOnCart,
+      required this.onIconTapped,
+      required this.isFavorite,
+      required this.addProductToCart,
+      required this.removeProductFromCart
+     });
+
+  final List<Product> favProducts;
+  final List<Product> productsOnCart;
+  final bool isProductsOnCart;
+  final int productsOnCartCount;
+  final void Function(Product) onIconTapped;
+  final bool Function(Product) isFavorite;
+  final void Function(Product, int) addProductToCart;
+  final void Function(Product, int) removeProductFromCart;
 
   @override
   State<CompleteProductList> createState() => _CompleteProductListState();
@@ -18,18 +42,58 @@ class _CompleteProductListState extends State<CompleteProductList> {
   late Future<List<Product>> futureFoodList;
   late Future<List<Product>> futureBeverageList;
   late Future<List<Product>> futureFashionList;
+  late Future<Users> futureUserData;
+  late bool _isProductsOnCart;
+  late int _productsOnCartCount;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  bool _isVisible = true;
+
 
   @override
   void initState() {
     super.initState();
+    _isProductsOnCart = widget.isProductsOnCart;
+    _productsOnCartCount = widget.productsOnCartCount;
     futureFoodList = service.retrieveFoodProducts();
     futureBeverageList = service.retrieveBeverageProducts();
     futureFashionList = service.retrieveFashionProducts();
   }
 
+  void _setIsProductOnCart() {
+    setState(() {
+      _isProductsOnCart = true;
+      _productsOnCartCount++;
+    });
+  }
+
+  void _afterProductRemoved(Product product) {
+    setState(() {
+      _productsOnCartCount--;
+      if (_productsOnCartCount < 1) {
+        _isProductsOnCart = false;
+      }
+    });
+  }
+
+  void _onStartScroll() {
+    setState(() {
+      _isVisible = false;
+    });
+  }
+
+  void _onStopScroll() {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _isVisible = true;
+      });
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        extendBody: true,
         appBar: AppBar(
           surfaceTintColor: Colors.white,
           leading: const Icon(Icons.arrow_back),
@@ -44,8 +108,7 @@ class _CompleteProductListState extends State<CompleteProductList> {
             child: Row(
               children: [
                 const SizedBox(width: 25),
-                const Icon(Icons.search,
-                    color: Color(0xFF9CA3AF), size: 30),
+                const Icon(Icons.search, color: Color(0xFF9CA3AF), size: 30),
                 const SizedBox(width: 5),
                 Text('Search',
                     style: GoogleFonts.inter(
@@ -58,56 +121,107 @@ class _CompleteProductListState extends State<CompleteProductList> {
             const SizedBox(width: 10)
           ],
         ),
+        bottomNavigationBar: _isProductsOnCart
+            ? ScrollToHideWidget(
+            isVisible: _isVisible,
+              child: Container(
+                alignment: Alignment.center,
+                height: 80,
+                color: Colors.white.withOpacity(0.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              ShoppingCart(productsOnCart: widget.productsOnCart,
+                                  removeProductFromCart: widget.removeProductFromCart,
+                                  afterProductRemoved: _afterProductRemoved)
+                      ));
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF314797),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 70, vertical: 5)),
+                    child: Text("$_productsOnCartCount products in cart",
+                        style: GoogleFonts.inter(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).colorScheme.background))),
+            )
+        )
+            : const SizedBox(),
         body: FutureBuilder(
-          future: Future.wait([futureFoodList, futureBeverageList, futureFashionList]),
-          builder: (context, AsyncSnapshot<List<List<Product>>> snapshot) {
-            if(!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator()
-              );
+          future: Future.wait(
+              [futureFoodList, futureBeverageList, futureFashionList]),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
             } else if (snapshot.hasError) {
               return Text('${snapshot.error}');
             }
             var foodList = snapshot.data![0];
             var beverageList = snapshot.data![1];
             var fashionList = snapshot.data![2];
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
-                  productCardHeader(context, 'Produk Makanan Kami!', () {}),
-                  StaticGrid(
-                      gap: 4,
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      children: [
-                        for (var i = 0; i < 4; i++)
-                          productCard(context, foodList[i])
-                      ]
-                  ),
-                  const SizedBox(height: 40),
-                  productCardHeader(context, 'Produk Minuman Kami!', () {}),
-                  StaticGrid(
-                    gap: 4,
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    children: [
-                      for (var i = 0; i < 4; i++)
-                        productCard(context, beverageList[i])
-                    ]
-                  ),
-                  const SizedBox(height: 40),
-                  productCardHeader(context, 'Produk Fashion Kami!', () {}),
-                  StaticGrid(
-                      gap: 4,
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      children: [
-                        for (var product in fashionList) productCard(context, product)
-                      ]
-                  )
-                ],
-              ),
+            return NotificationListener<ScrollNotification>(
+                onNotification: (scrollNotification) {
+                  if (scrollNotification is ScrollStartNotification) {
+                    _onStartScroll();
+                  } else if (scrollNotification is ScrollEndNotification) {
+                    _onStopScroll();
+                  }
+                  return true;
+                },
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    productCardHeader(context, 'Produk Makanan Kami!', () {}),
+                    StaticGrid(
+                        gap: 4,
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        children: [
+                          for (var i = 0; i < 4; i++)
+                            productCard(
+                                context, foodList[i], widget.productsOnCart,
+                                widget.isFavorite(foodList[i]),
+                                widget.onIconTapped, widget.addProductToCart,
+                                _setIsProductOnCart
+                            )
+                        ]),
+                    const SizedBox(height: 40),
+                    productCardHeader(context, 'Produk Minuman Kami!', () {}),
+                    StaticGrid(
+                        gap: 4,
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        children: [
+                          for (var i = 0; i < 4; i++)
+                            productCard(
+                                context, beverageList[i], widget.productsOnCart,
+                                widget.isFavorite(beverageList[i]),
+                                widget.onIconTapped, widget.addProductToCart,
+                                _setIsProductOnCart
+                            )
+                        ]),
+                    const SizedBox(height: 40),
+                    productCardHeader(context, 'Produk Fashion Kami!', () {}),
+                    StaticGrid(
+                        gap: 4,
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        children: [
+                          for (var fashionProduct in fashionList)
+                            productCard(
+                                context, fashionProduct, widget.productsOnCart,
+                                widget.isFavorite(fashionProduct),
+                                widget.onIconTapped, widget.addProductToCart
+                            )
+                        ]),
+                    const SizedBox(height: 90)
+                  ],
+                ),
+              )
             );
           },
-        )
-    );
+        ));
   }
 }
