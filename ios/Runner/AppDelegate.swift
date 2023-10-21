033f5f3789fdff8dd4f3f53656c0db26072a81f8
Guspascad420 <Paacad@gmail.com>
2023-10-21 19:27:47 +0700

    Added google maps and location

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    [GMSServices provideAPIKey:@"AIzaSyBylW7d_eEFq7i9nFVRo9x_n4jQTk9wc2M"];
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
