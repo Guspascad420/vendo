3edc95486034b4fd3cb5f3a200ce37d327a380f9
Guspascad420 <Paacad@gmail.com>
2023-10-21 19:28:55 +0700

    Added currency formatter

---
+++ lib/utils/currency_format.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+
+class CurrencyFormat {
+  static String convertToIdr(dynamic number) {
+    NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: 'id',
+      symbol: 'Rp ',
+      decimalDigits: 0,
+    );
+    return currencyFormatter.format(number);
+  }
+  static basicConversion(dynamic number) {
+    NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: 'id',
+      symbol: '',
+      decimalDigits: 0,
+    );
+    return currencyFormatter.format(number);
+  }
+}
