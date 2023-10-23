e0c1e6212bcdcf0b2cfd84b7e9369a9e026c54a9
Guspascad420 <Paacad@gmail.com>
2023-10-23 18:36:24 +0700

    Added phone number property

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -3,19 +3,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Users {
   final String fullName;
   final String email;
+  final String phoneNumber;
   List<Map<String, dynamic>> productsOnCart;
   List<Map<String, dynamic>> favProducts;
 
-  Users(
-      {required this.fullName,
+  Users({required this.fullName,
       required this.email,
       required this.productsOnCart,
-      required this.favProducts});
+      required this.favProducts, required this.phoneNumber});
 
   Map<String, dynamic> toMap() {
     return {
       'full_name': fullName,
       'email': email,
+      'phone_number': phoneNumber,
       'products_on_cart': productsOnCart,
       'fav_products': favProducts
     };
@@ -24,6 +25,7 @@ class Users {
   Users.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
       : fullName = doc.data()!["full_name"],
         email = doc.id,
+        phoneNumber =  doc.data()!["phone_number"],
         productsOnCart = doc.data()?["products_on_cart"].cast<Map<String, dynamic>>(),
         favProducts = doc.data()?["fav_products"].cast<Map<String, dynamic>>();
 }
