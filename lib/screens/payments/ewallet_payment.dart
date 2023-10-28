f8f6d9610d53215c1cedd295b1a94feaaf07eed9
Guspascad420 <Paacad@gmail.com>
2023-10-28 12:58:41 +0700

    Added method to handle completed payment

--- lib/screens/payments/ewallet_payment.dart
+++ lib/screens/payments/ewallet_payment.dart
@@ -1,30 +1,63 @@
 import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:randomstring_dart/randomstring_dart.dart';
+import 'package:vendo/models/order.dart';
 import 'package:vendo/screens/payments/payment_completed.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:http/http.dart' as http;
 
 import '../../models/category.dart';
+import '../../database/database_service.dart';
+import '../../models/product.dart';
 
 class EWalletPayment extends StatefulWidget {
   const EWalletPayment({super.key,
       required this.totalCost,
       required this.deeplinkRedirect,
       required this.transactionId,
-      required this.productCategory});
+      required this.productCategory,
+      required this.productsOnCart});
 
   final int totalCost;
   final String deeplinkRedirect;
   final String transactionId;
   final Category productCategory;
+  final List<Product> productsOnCart;
 
   @override
-  State<StatefulWidget> createState() => _EWalletPaymentState();
+  State<EWalletPayment> createState() => _EWalletPaymentState();
 }
 
 class _EWalletPaymentState extends State<EWalletPayment> {
   late final WebViewController _controller;
+  final rs = RandomString();
+  DatabaseService service = DatabaseService();
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String _uniqueCode = "";
+
+  Future<void> _handleCompletedPayment(String id, List<Product> productsOnCart) async {
+    for (var product in widget.productsOnCart) {
+      service.removeProductFromCart(auth.currentUser!.uid, product,
+          product.quantity!);
+    }
+    _createNewOrder();
+  }
+
+  void _createNewOrder() {
+    setState(() {
+      _uniqueCode = rs.getRandomString(lowersCount: 0, uppersCount: 3,
+          specialsCount: 0).substring(0, 4);
+    });
+    String productCategory = widget.productCategory == Category.foodOrBeverage
+        ? "F&B" : "Fashion";
+    Order order = Order(uniqueCode: _uniqueCode,
+        price: widget.totalCost, category: productCategory,
+        status: "Sukses", userId: auth.currentUser!.uid);
+    service.addOrder(order);
+  }
+
 
   @override
   void initState() {
@@ -35,18 +68,23 @@ class _EWalletPaymentState extends State<EWalletPayment> {
       ..setNavigationDelegate(
         NavigationDelegate(
           onNavigationRequest: (NavigationRequest request) {
-            http.get(Uri.parse('http://192.168.18.42:8080/api/status/'
+            http.get(Uri.parse('https://midtrans-go-api--6h08mix.lemonpond-99927c12.'
+                'southeastasia.azurecontainerapps.io/api/status/'
                     '${widget.transactionId}')).then((response) {
               if (response.statusCode == 200) {
                 var data = jsonDecode(response.body) as Map<String, dynamic>;
                 if (data["status"] == "settlement") {
-                  Navigator.of(context).push(
+                  _handleCompletedPayment(auth.currentUser!.uid, widget.productsOnCart)
+                      .then((value) => {
+                    Navigator.of(context).push(
                       MaterialPageRoute(
-                          builder: (context) => PaymentCompleted(
-                              productCategory: widget.productCategory
-                          )
+                        builder: (context) => PaymentCompleted(
+                          productCategory: widget.productCategory,
+                          uniqueCode: _uniqueCode,
+                        )
                       )
-                  );
+                    )
+                  });
                 }
               } else {
                 throw Exception('Failed to post request');
